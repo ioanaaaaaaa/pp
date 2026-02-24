f49171092d8af185ecb128c26eee6e69d8284c0b
ioanaaaaaaa <ioana-petronela.avasiloaie@student.tuiasi.ro>
2026-02-24 20:16:45 +0200

    citire fisier date.txt

---
+++ lab1/pb2/script.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+text_initial = citeste_fisier('date.txt')
+# Deocamdata doar afisam textul brut
+print("--- Text Brut ---")
+print(text_initial)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
